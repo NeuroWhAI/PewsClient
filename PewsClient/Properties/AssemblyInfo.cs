cedd069ed0c0239382cc0c7fa9f2e2a8e3f85a7d
NeuroWhAI <tlsehdgus0212@gmail.com>
2021-01-05 21:57:24 +0900

    Release v1.9.0

--- PewsClient/Properties/AssemblyInfo.cs
+++ PewsClient/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("PewsClient")]
-[assembly: AssemblyCopyright("Copyright © NeuroWhAI 2020")]
+[assembly: AssemblyCopyright("Copyright © NeuroWhAI 2021")]
 [assembly: AssemblyTrademark("NeuroWhAI")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // 모든 값을 지정하거나 아래와 같이 '*'를 사용하여 빌드 번호 및 수정 번호를
 // 기본값으로 할 수 있습니다.
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.8.2.0")]
-[assembly: AssemblyFileVersion("1.8.2.0")]
+[assembly: AssemblyVersion("1.9.0.0")]
+[assembly: AssemblyFileVersion("1.9.0.0")]
